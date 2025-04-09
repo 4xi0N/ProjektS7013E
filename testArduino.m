8e9bb6141c67b8dc706a0d68bb957c6f117b5319
4xi0N <nipsan175@gmail.com>
2025-04-09 14:18:09 +0200

    Add files via upload
    Ny kod för temperaturmätaren efter 9/4-25

--- testArduino.m
+++ testArduino.m
@@ -3,8 +3,14 @@ s = serialport('COM3',115200);
 voltage_array = [];
 while true
     voltage_data = str2double(readline(s));
-    voltage = voltage_data*3.3/4095;
-    voltage_array = [voltage_array,voltage];
-    plot(1:length(voltage_array),voltage_array,'r-');
+    voltage = voltage_data * 3.3 / 4095;
+    voltage_array = [voltage_array, voltage];
+    
+    % Create time vector assuming each sample is 0.1 seconds apart
+    t = (0:length(voltage_array)-1) * 0.1; 
+    
+    plot(t, voltage_array, 'r-');
+    xlabel('Time (seconds)');
+    ylabel('Voltage (V)');
+    drawnow; % Ensures the plot updates in real time
 end
-
