f5193491a804afdd4e816f1f9305e00402f7297b
4xi0N <nipsan175@gmail.com>
2025-05-12 09:22:03 +0200

    Update temperatur_measure_diff_amp.ino

--- temperatur_measure_diff_amp.ino
+++ temperatur_measure_diff_amp.ino
@@ -1,19 +1,18 @@
 
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(A0,INPUT);
-  pinMode(13,OUTPUT);
-  digitalWrite(13,HIGH);
-  Serial.begin(115200);
-  analogReadResolution(12);
+  pinMode(A0,INPUT); // input pin
+  pinMode(13,OUTPUT); // output pin
+  digitalWrite(13,HIGH); // pin 13, max voltage
+  Serial.begin(115200); // bits per second (baud-rate)
+  analogReadResolution(12); // bit size range 0-4095
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int voltage = analogRead(A0);
-  Serial.print(voltage);
-  Serial.println();
+  int voltage = analogRead(A0); // read the A0 pin
+  Serial.print(voltage); // print value to serial port in ASCII text
+  Serial.println(); // end print with newline
 
-  delay(100);
+  delay(100); // 100 ms delay before next loop
 }
